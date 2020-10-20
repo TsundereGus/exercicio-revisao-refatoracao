7cf8e50b1c8ea2f845fe0bdf942bea4dba6c2eb2
Gustavo Rodrigues <42501788+TsundereGus@users.noreply.github.com>
2020-10-20 20:02:07 -0300

    Update Vendedor.cpp

--- Vendedor.cpp
+++ Vendedor.cpp
@@ -1,14 +1,21 @@
-#include <string>
-#include "Empregado.hpp"
+#include "Vendedor.hpp"
 
-class Vendedor : public Empregado {
+#define MESES_DO_ANO 12
 
-  public:
-    std::string nome;  	  
-	
-	double quotaTotalAnual() {
-	  return quotaMensalVendas * 12;
-	}
-	
-};
+double Vendedor::quotaTotalAnual() {
+	return getQuotaMensalVendas() * MESES_DO_ANO;
+}
 
+void Vendedor::print(double hTrab){
+	std::cout << "Nome: " << this->getNome() << std::endl;
+  	std::cout << "Salario Mes: " << this->pagamentoMes(hTrab) << std::endl;
+  	std::cout << "Quota vendas: " << quotaTotalAnual() << std::endl; 
+  	std::cout << std::endl;
+}
+
+void Vendedor::collectData(string collNome, double collSalario, int collProjetos, double collHTrab){
+  setNome(collNome);
+  setSalarioHora(collSalario);
+  setQuotaMensalVendas(collProjetos);
+  print(collHTrab);
+}
