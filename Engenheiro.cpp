beaa969cde3f5aaf727db1e7c7006f1dc51f2b4d
Gustavo Rodrigues <42501788+TsundereGus@users.noreply.github.com>
2020-10-20 20:02:39 -0300

    Update Engenheiro.cpp

--- Engenheiro.cpp
+++ Engenheiro.cpp
@@ -1,11 +1,14 @@
-#include <string>
 #include "Empregado.hpp"
 
-class Engenheiro : public Empregado {
+#define HORAS_DIARIAS 8
 
-  public:
-    std::string nome;  
-	int projetos;
-	
-};
-
+double Empregado::pagamentoMes(double horasTrabalhadas){    
+        double salarioMes = horasTrabalhadas;
+        
+        //Cálculo de hora extra (+50% se horasTrabalhadas > 8)
+        if (horasTrabalhadas > HORAS_DIARIAS){
+            double horasExtras = horasTrabalhadas - HORAS_DIARIAS;
+            salarioMes += horasExtras / 2;
+        }
+        return salarioMes * salarioHora;
+}
